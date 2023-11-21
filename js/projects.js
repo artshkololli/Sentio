b51e9be5e36f707be80d0b17c1efeb4d7f08fa11
Art <artshkololli@gmail.com>
2023-11-21 22:31:46 +0100

    Modal Slider

--- js/projects.js
+++ js/projects.js
@@ -38,3 +38,43 @@ const filterCards = e =>{
 }
 
 filterButtons.forEach(button =>button.addEventListener("click",filterCards));
+
+//Image Modal
+var modal = document.getElementById("myModal");
+var img = document.getElementById("myImg");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+//Slider Modal
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+  }
