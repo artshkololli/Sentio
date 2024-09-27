57683b3a4a0502dad0d6bff4f5a04783c88300e5
Art <artshkololli@gmail.com>
2024-09-27 19:11:00 +0200

    Redirecting fix

--- index.js
+++ index.js
@@ -33,3 +33,21 @@ hamburger.addEventListener("click", ()=>{
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+//Fade in Animation
+
+const observer=new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    console.log(entry);
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  })
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>{
+  observer.observe(el);
+})
